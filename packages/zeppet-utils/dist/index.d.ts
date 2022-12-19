27d3a0eac31ada997a6854680a61db7fc57e5bb7
Nikita Tyrkin <tyrkinn@pm.me>
2022-12-19 22:17:18 +0300

    Remove dist folders

--- packages/zeppet-utils/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Action } from '@zeppet/core';
-
-declare const voidAction: <T extends HTMLElement>(voidFn: (element: T) => void) => Action<T>;
-declare const sideEffectAction: <T extends HTMLElement>(sideEffect: () => void) => Action<T>;
-
-export { sideEffectAction, voidAction };
