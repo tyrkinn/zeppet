3dc1cbca42179f618d87107346d43988bfdcb5e4
Nikita Tyrkin <tyrkinn@pm.me>
2022-12-20 16:50:20 +0300

    Add doc commend to compose function

--- packages/zeppet-core/src/core/actions/compose.ts
+++ packages/zeppet-core/src/core/actions/compose.ts
@@ -1,6 +1,32 @@
 import { Action } from "$types";
 import { ComposeFn } from "$types";
 
+/**
+  * Function that compose your actions into one action
+  *
+  * @typeParam T - HTMLElement kind of composed Action
+  *
+  * @remarks
+  * Provide type parameter if you want your action to be used only for this type of elementsh
+  *
+  * @param actions - actions to compose
+  *
+  * @example
+  * Basic compose
+  * ```
+  * // This example use actions from @zeppet/actions
+  *
+  * const useButton = () => {
+  *   const textObs = observe("Button text");
+  *   return compose(
+  *     bindFieldToObserver('textContent', textObs),
+  *     mutateOnEvent('click', textObs, (prev) => prev + "!")
+  *   )
+  * }
+  * ```
+  * 
+  * @returns New action that includes all composed actions
+  */
 export const compose: ComposeFn = <T extends HTMLElement>(...actions: Action<T>[]) => {
   return actions.reduceRight((acc, fn) => ((el: T) => fn(acc(el))) as Action<T>)
 }
